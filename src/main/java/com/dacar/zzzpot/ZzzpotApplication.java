bc22824fe0c53230426dc3ea40dd48d1aa593665
David Rodríguez Escobar <50140333+Davidrodrguez98@users.noreply.github.com>
2026-05-11 21:29:36 +0200

    Initial commit

---
+++ src/main/java/com/dacar/zzzpot/ZzzpotApplication.java
@@ -0,0 +1,13 @@
+package com.dacar.zzzpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzzpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzzpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dacar/zzzpot/ZzzpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacar.zzzpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzzpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
