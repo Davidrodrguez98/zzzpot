5a03e8254338702121658d2f0a2bbeb4c6386f7f
Carmen <67647493+carmengl00@users.noreply.github.com>
2026-05-17 21:44:18 +0200

    feat: Environment configuration

--- src/test/java/com/dacar/zzzpot/ZzzpotApplicationTests.java
+++ src/test/java/com/dacar/zzzpot/ZzzpotApplicationTests.java
@@ -2,8 +2,10 @@ package com.dacar.zzzpot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ZzzpotApplicationTests {
 
 	@Test
